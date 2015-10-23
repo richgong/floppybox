9148fa1907258efe9accfa7a6f2ce4d865b1e2de
Richard Gong <richardgong@gmail.com>
2015-10-22 20:52:12 -0700

    deploy scripts for baylaunch

---
+++ _include.sh
@@ -0,0 +1,5 @@
+export DOMAINS="floppybox.baylaunch.com"
+export AWS_HOST=3
+export HEROKU_APP="floppybox"
+
+source ../recbro/lib/_include.sh || echo "no include"
---
+++ bin/post_sync.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -veo pipefail # return exit code of failure, if anything fails
+
+sudo npm install --production
